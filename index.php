7c3fb2ce51bbefce5aa12d31348c75c169947ab0
artemlein <artem_reshetnik@mail.ru>
2019-01-28 20:09:21 +0300

    asdasd

--- index.php
+++ index.php
@@ -6,5 +6,15 @@
 </head>
 <body>
 	<?php echo "hello world"; ?>
-</body>
+		<script type="text/javascript" src="https://vk.com/js/api/openapi.js?160"></script>
+<script type="text/javascript">
+  VK.init({apiId: API_ID});
+</script>
+
+<!-- VK Widget -->
+<div id="vk_auth"></div>
+<script type="text/javascript">
+  VK.Widgets.Auth("vk_auth", {"authUrl":"/dev/Login"});
+</script>
+	</body>
 </html>
