3267759a829728460147b345c81f60ba9259ff61
artemlein <artem_reshetnik@mail.ru>
2019-01-29 14:23:42 +0300

    sdadwq

--- index.php
+++ index.php
@@ -6,23 +6,9 @@
 	<script type="text/javascript" src="https://vk.com/js/api/openapi.js?160"></script>
 </head>
 <body>
-	<?php require 'stemauth/steamauth.php'l
-	loginButton();
 
-	if(isset($_SESSION['steamid'])){
-		require 'steamauth/userInfo.php';
-		echo $steamprofile['personaname'];
-		echo "<a href='stemauth/logout.php'>Выйти</a>"
-	}
-	?>
 
 	
-
-
-	<form action="?login" method="post">
-    	<input type="image" src="http://cdn.steamcommunity.com/public/images/signinthroughsteam/sits_small.png">
-	</form>
-
 
 
 		
